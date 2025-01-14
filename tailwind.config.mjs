4e762d6467e38cb1246119d692929acc0b06f021
Gonzalo Fleming <gonzafg2@gmail.com>
2025-01-14 00:42:47 -0300

    feat: Translate text to spanish

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["SF Pro Display", "system-ui", "sans-serif"],
+        sans: ['SF Pro Display', 'system-ui', 'sans-serif'],
       },
     },
   },
